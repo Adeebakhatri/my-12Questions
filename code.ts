a9efc01f0ae62cd5e81b2eeb428007e0064550d7
Adeebakhatri <160527402+Adeebakhatri@users.noreply.github.com>
2024-02-26 21:00:47 +0500

    Add files via upload

---
+++ code.ts
@@ -0,0 +1,117 @@
+//exercise 2
+//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+//let personName = "Eric";
+//console.log(`hello ${personName}, would you like to learn typescript today?`);
+
+
+
+//exercise 3
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+//lower case
+//let person_Name: string="M.Aun"
+//console.log("lower case:","person_Name.toLowerCase()");
+
+//upper case
+//console.log("upper case:","person_Name.toUpperCase()");
+
+//tital case
+//console.log("titalcase:",person_Name.replace(/\bwlg,c=>c.toUpperCase()));
+
+
+
+
+//exercise 4
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+ 
+//let quote: string= "I do not believe in taking the right decision, I take a decision and make it right";
+//let author: string= "Muhammad Ali Jinnah";
+//console.log(`${author} once said, "${quote}`);
+
+
+//exercise 5
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+// let quote: string="I do not believe in taking the right decision, I take a decision and make it right";
+// let famous_person: string= "Muhammad Ali Jinnah";
+// let message=`${famous_person} once said, ${quote}`;
+// console.log(message);
+
+
+
+//exercise 6
+// Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+// let personName: string = '\n\t ADEEL KHATRI\t\n';
+// console.log(personName);
+// let stripped: string = personName.trim();
+// console.log(stripped)
+
+
+//exercise 7,8
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.You should create four lines that look like this:console.log(5 + 3)
+
+// console.log(4+4);
+// console.log(10-2);
+// console.log(2*4);
+// console.log(24/3);
+
+
+
+//exercise 9
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+// let favoriteNumber: number = 8;
+// console.log(`My favorite Number is ${favoriteNumber}`);
+
+
+
+
+//EXERCISE 10
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+//exercise 10
+//my name is Adeeba 
+//Date:24/2/2024
+//this program will run simple code e.g:hello world 
+//console.log('Hello world simple program');
+
+//evercise 10
+//My name is Adeeba
+//Date:24/2/2024
+//this program will do multiplication 
+//console.log(5*2);
+
+
+
+//EXERCISE 11
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+// let member: string[]=['sara','sana','azkia','fatima'];
+// for(let i=0; i<member.length; i++){
+//     console.log(member[i]);
+// }
+
+
+
+//EXERCISE 12
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+// let members: string[] = ['sara','sana','azkia','fatima'];
+// let message: string = 'Hello how are you:';
+// for(let i=0; i<members.length; i++) {
+//     console.log(message + members [i]);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
